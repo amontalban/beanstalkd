b17876f9e4db36271cc379728b2d05c93edd750e
Serafeim Zanikolas <serzan@hellug.gr>
2009-11-20 18:29:06 +0100

    Imported Upstream version 1.4.2

---
+++ debian/beanstalkd.default.ex
@@ -0,0 +1,10 @@
+# Defaults for beanstalkd initscript
+# sourced by /etc/init.d/beanstalkd
+# installed at /etc/default/beanstalkd by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
